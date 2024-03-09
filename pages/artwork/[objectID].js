5e779df12822217e5f6f5bbf57af5176e5697e59
XxmangreatxX <you@example.com>
2024-03-08 22:45:58 -0500

    Fourth update

--- pages/artwork/[objectID].js
+++ pages/artwork/[objectID].js
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { Row, Col } from 'react-bootstrap';
-import ArtworkCardDetail from '../../components/ArtworkCardDetail';
+import ArtworkCardDetail from '../../components/ArtworkCard.js';
 
 const ArtworkById = () => {
   const router = useRouter();
